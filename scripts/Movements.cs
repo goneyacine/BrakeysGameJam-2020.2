66ec610ce9100af122ebddfbb217af54e5c3dff5
goneyacine <52914216+goneyacine@users.noreply.github.com>
2020-08-02 00:42:44 +0200

    i created screenBox system and added player movement

---
+++ scripts/Movements.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    //the horizontal speed of the player
+    public float speed;
+   //getting the rigidbody2d (rb) component
+    void Start() { rb = GetComponent<Rigidbody2D>(); }
+
+   //doing the movement things 
+    void Update()
+    {
+        float moveInputValue = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInputValue * speed * Time.deltaTime,rb.velocity.y);
+    }
+}
---
+++ scripts/ScreenBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+[ExecuteInEditMode]
+public class ScreenBox : MonoBehaviour
+{
+  public CinemachineVirtualCamera cinemachineCamera;
+  private BoxCollider2D collider2D;
+  public string playerTag = "Player";
+  public int defaultCamPriority = 0;
+  public int onSelectCamPriority = 10;
+  
+  //getting the boxCollider2D component 
+   private void Start() {collider2D = GetComponent<BoxCollider2D>();}
+
+//draw the box collider as gizmos
+  private void OnDrawGizmos()
+   { 
+      Gizmos.color = Color.blue;
+      Gizmos.DrawWireCube((Vector2)transform.position + collider2D.offset,collider2D.size);
+   }
+ private void OnTriggerEnter2D(Collider2D other) {
+     if(other.tag == playerTag)
+      cinemachineCamera.Priority = onSelectCamPriority;
+ }
+ private void OnTriggerExit2D(Collider2D other) {
+     if(other.tag == playerTag)
+     cinemachineCamera.Priority = defaultCamPriority;
+ }
+}
