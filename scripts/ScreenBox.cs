c45de3a3fb145a21432000c893627601fa6527a5
goneyacine <52914216+goneyacine@users.noreply.github.com>
2020-08-06 08:25:43 +0200

    adding some sound effect

--- scripts/ScreenBox.cs
+++ scripts/ScreenBox.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+
 [ExecuteInEditMode]
 public class ScreenBox : MonoBehaviour
 {
---
+++ scripts/dialog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class dialog : MonoBehaviour
+{
+  public List<string> charactersNames;
+  public List<string> dialogs;
+  public float typingDelay = .4f;
+  public float delayBetweenDialogv = 2f;
+  private string currentDialogText;
+  private string currentCharacterName;
+  public Text characterNameText;
+  public Text dialogText;
+  public UnityEvent dialogEndEvent;
+  private void OnEnable(){
+      StartCoroutine(dialogCoroutine());
+  }
+  public string getCurrentDialogText(){ return currentDialogText; }
+  public string getCurrentCharacterName(){ return currentCharacterName; }
+  IEnumerator dialogCoroutine(){
+      for(int i = 0; i < dialogs.Count;i++){
+       currentCharacterName = charactersNames[i];
+            characterNameText.text = getCurrentCharacterName();
+            currentDialogText = "";
+       foreach(char char_ in dialogs[i]){
+        currentDialogText += char_;
+        yield return new WaitForSeconds(typingDelay);
+                dialogText.text = getCurrentDialogText();
+       }
+       yield return new WaitForSeconds(delayBetweenDialogv);
+      }
+        dialogEndEvent.Invoke();
+  }
+}
